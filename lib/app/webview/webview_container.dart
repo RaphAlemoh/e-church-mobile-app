54f01d12e0bfa57cef529cda5bd75b545a7176ae
Alemoh Rapheal <raphealenike@gmail.com>
2022-03-08 05:35:51 -0800

    flutter webview app using very good adventures

---
+++ lib/app/webview/webview_container.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewContainer extends StatefulWidget {
+  const WebViewContainer({
+    Key? key,
+    required this.url,
+    required this.title,
+  }) : super(key: key);
+
+  final String url;
+  final String title;
+
+  @override
+  WebViewContainerState createState() => WebViewContainerState();
+}
+
+class WebViewContainerState extends State<WebViewContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: WebView(
+              key: widget.key,
+              javascriptMode: JavascriptMode.unrestricted,
+              initialUrl: widget.url,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
