b2648366900881813044f8157a9486b5adb4affa
Alemoh Rapheal <raphealenike@gmail.com>
2022-03-12 03:41:23 -0800

    Webview app updates

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -7,7 +7,6 @@
 
 import 'package:echurch/app/webview/webview_container.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
 
 class App extends StatelessWidget {
@@ -17,17 +16,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: ThemeData(
-      //   appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-      //   colorScheme: ColorScheme.fromSwatch(
-      //     accentColor: const Color(0xFF13B9FF),
-      //   ),
-      // ),
-      localizationsDelegates: const [
-        // AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      // supportedLocales: AppLocalizations.supportedLocales,
       home: SplashScreenView(
         navigateRoute: const MyApp(),
         duration: 10000,
